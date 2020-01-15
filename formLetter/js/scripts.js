e9485ae7c7d12622dc83b01c8f0f861da1553921
BrevinCronk98 <59577353+BrevinCronk98@users.noreply.github.com>
2020-01-15 08:51:02 -0800

    Add files via upload

---
+++ formLetter/js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $("#formBox").submit(function(event) {
+
+        var nameInput = $("input#name").val();
+
+        $(".name").text(nameInput);
+
+        $("#letterText").show();
+
+        event.preventDefault();
+    })
+});
